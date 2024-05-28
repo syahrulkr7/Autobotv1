d77bff54edacd350c807e37772edbb709dacd2e7
RhythmMusic <168445063+RhythmMusic@users.noreply.github.com>
2024-05-28 23:27:16 +0800

    Update ai.js

--- script/ai.js
+++ script/ai.js
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
             const generatedText = response.data.response;
 
             // Ai Answer Here
-            api.sendMessage(`𝗚𝗣𝗧 3.5 𝗧𝗨𝗥𝗕𝗢 𝗠𝗢𝗗. \n━━━━━━━━━━━━━━━━━━\n\${generatedText}\n━━━━━━━━━━━━━━━━━━`, event.threadID, messageID);
+            api.sendMessage(`𝗚𝗣𝗧 3.5 𝗧𝗨𝗥𝗕𝗢 𝗠𝗢𝗗. \n━━━━━━━━━━━━━━━━━━\n${generatedText}\n━━━━━━━━━━━━━━━━━━`, event.threadID, messageID);
         } else {
             console.error('API response did not contain expected data:', response.data);
             api.sendMessage(`❌ An error occurred while generating the text response. Please try again later. Response data: ${JSON.stringify(response.data)}`, event.threadID, messageID);
